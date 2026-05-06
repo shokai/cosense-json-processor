1b678f78ce39cc11404c557c7f52daee8a1ed4b6
Sho Hashimoto <hashimoto@shokai.org>
2026-05-07 00:44:31 +0900

    npm run buildでout/に出力する

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: './',
   typescript: {
     ignoreBuildErrors: true,
   },
